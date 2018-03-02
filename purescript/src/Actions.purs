c96b3f396aea9e3ed2972fd5091bd2ff8d5cd111
Patrick Martin <bypatrickmartin@gmail.com>
2018-03-02 00:12:19 -0800

    Compiles casesFn

--- purescript/src/Actions.purs
+++ purescript/src/Actions.purs
@@ -12,6 +12,7 @@ module Ngrx.Actions
 import Prelude
 import Data.String (length, take)
 
+
 newtype Payload p = Payload p
 
 type Success p r =
@@ -28,7 +29,6 @@ type Action p m =
     , meta    :: m
     , error   :: Boolean }
 
-
 type ActionCreator p = forall m. p -> m -> Action p m
 
 type AsyncActionCreator p r e =
@@ -37,9 +37,8 @@ type AsyncActionCreator p r e =
     , failed :: ActionCreator (Failure p e) }
 
 
-
 isType :: forall p m. String -> Action p m -> Boolean
-isType at a = a.type == take (length a.type) at
+isType t a = a.type == take (length a.type) t
 
 
 createAction :: forall p. String -> ActionCreator p
--- purescript/src/Reducers.purs
+++ purescript/src/Reducers.purs
@@ -1,13 +1,12 @@
 module Ngrx.Reducers where
 
-import Prelude
+-- import Prelude
 import Data.Array (foldr)
 
-import Ngrx.Actions (Action, ActionCreator, Payload, isType)
+import Ngrx.Actions (Action, Payload, isType)
 
 type Reducer s p m = s -> Action p m -> s
 type Handler s p = s -> Payload p -> s
-
 
 
 caseFn :: forall s p m. String -> Handler s p -> Reducer s p m
@@ -15,10 +14,10 @@ caseFn t h = \s a -> if isType t a then h s a.payload else s
 
 
 casesFn :: forall s p m. Handler s p -> Array String -> Reducer s p m
-casesFn h = foldr (\acc ac -> \s a -> acc (caseFn ac h) s a) id
+casesFn h = foldr (\t acc -> \s a -> acc ((caseFn t h) s a) a) (\s a -> s)
 
 
-reducerDefaultFn = true
+-- reducerDefaultFn = true
 
 
-reducerFn = true
+-- reducerFn = true
